7b43833fb03134577ee13713728b6aaca2b02066
railization <mab@railization.com>
2014-12-23 19:16:15 +0100

    malier added if old user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     def create
         # Get user to see if they have already signed up
         @user = User.find_by_email(params[:user][:email]);
-
+        UserMailer.signup_email(@user).deliver
         # If user doesnt exist, make them, and attach referrer
         if @user.nil?
 
