d49cc7dbca022aefa4dad12cd6f8e6b3cbe6b7e3
railization <mab@railization.com>
2014-12-23 20:53:35 +0100

    deliver erors added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Prelaunchr::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+  config.action_mailer.raise_delivery_errors = true
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
